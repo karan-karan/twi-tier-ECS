19c13d7c008931d02888bb619a3f76c7847bc6c2
karan <karantamer75@gmail.com>
2026-02-10 16:24:22 +0530

    code updated

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 db.connect((err) => {
