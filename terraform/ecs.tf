8903cb2ebf8a3b42004c6ae31b7947bb969cf6fa
karan <karantamer75@gmail.com>
2026-02-16 02:41:06 +0530

    code

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_task_definition" "app" {
   network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  execution_role_arn       = data.aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
     {
